e9a7cccf02de91c9a1967ad401c8ca31b157a1ab
Veni13 <61907726+Veni13@users.noreply.github.com>
2020-05-13 10:15:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+}
+
+    function draw(){
+        background(255,255,255);
+        Engine.update(engine);
+    }
